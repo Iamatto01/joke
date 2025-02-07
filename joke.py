fc14bd248dcee9bae04e2a7fd4f1adfd38a1c434
MUHAMMAD SAIFUDIN BIN MOHD JAMAL <145031320+Iamatto01@users.noreply.github.com>
2025-02-08 02:36:42 +0800

    Update joke.py

--- joke.py
+++ joke.py
@@ -1,7 +1,27 @@
 import pyjokes
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
 
 def get_random_joke():
     return pyjokes.get_joke()
 
+@app.route('/')
+def home():
+    joke = get_random_joke()
+    return render_template_string('''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Random Joke Generator</title>
+        </head>
+        <body>
+            <h1>Random Programming Joke</h1>
+            <p>{{ joke }}</p>
+            <button onclick="window.location.reload();">Get Another Joke</button>
+        </body>
+        </html>
+    ''', joke=joke)
+
 if __name__ == "__main__":
-    print(get_random_joke())
+    app.run(debug=True)
